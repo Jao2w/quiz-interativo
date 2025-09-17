d36bb88eaaeb7ae23210cf1c6f9bc2f065a17df6
Jao2w <joao.lima.portes@escola.pr.gov.br>
2025-09-17 07:43:22 -0300

    .

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,1 +1,8 @@
+const nomes = ["Juca", "Emerson", "Osvaldo", "Pipokinha", "Deolane", "Virginia"];
 
+export function aleatorio (lista){
+    const posiçao = math.floor(math.random()* lista.length);
+    return lista[posiçao];
+}
+
+export const nome = aleatorio(nomes)
